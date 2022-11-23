8b3745b0dc453b724f7ee6fb502fbaa9ca57456a
Darya_Savitskaya <statusqvogerz@gmail.com>
2022-11-23 16:28:06 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function getSumOfSequence(num) {
+    let finArr = []
+    for (let i = 1; i <= num; i++) {
+        finArr.push(i) }
+
+    const finalSum = finArr[0] + num
+    return console.log(finArr,finalSum)
+}
+
+getSumOfSequence(5)
+
+
